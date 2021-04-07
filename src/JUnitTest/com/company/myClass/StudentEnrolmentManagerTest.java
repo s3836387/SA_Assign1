2cc21cd865004af26e7e73d0b3556fa27f6fa1f6
Khai Ngo <dothat0@gmail.com>
2021-04-07 14:21:36 +0700

    modifying and fixing input validation function

--- src/JUnitTest/com/company/myClass/StudentEnrolmentManagerTest.java
+++ src/JUnitTest/com/company/myClass/StudentEnrolmentManagerTest.java
@@ -11,9 +11,9 @@ class StudentEnrolmentManagerTest {
     dataObjectFactory data = new dataObjectFactory();
     Student student =new Student("s3836387","Ngo Quang khai", LocalDate.parse("2000-12-02"));
     Course course =new Course("COSC2440","Software Architecture Design and Implementation",12);
+    StudentEnrolmentManager manager = new StudentEnrolmentManager();
     @Test
     void add() {
-        StudentEnrolmentManager manager = new StudentEnrolmentManager();
         boolean output = manager.add(student,course,"2021A");
         assertTrue(output);
         output = manager.add(student,course,"2021A");
--- src/com/company/myClass/Console.java
+++ src/com/company/myClass/Console.java
@@ -1,7 +1,5 @@
 package com.company.myClass;
 
-import com.company.myPackage.Lead;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.List;
@@ -11,12 +9,13 @@ public class Console {
 
     private static final Scanner scanner = new Scanner(System.in);
     private Console() {}
-    public static String charIn(String prompt) {
+
+    public String charIn(String prompt) {
         System.out.print(prompt);
         return scanner.next();
     }
 
-    public static int intIn(String prompt) {
+    public int intIn(String prompt) {
         System.out.print(prompt);
         return scanner.nextInt();
     }
@@ -27,46 +26,47 @@ public class Console {
         System.out.print(prompt);
         int input;
         while (!scanner.hasNextInt()) {
-            System.out.print(prompt);
+            System.out.print("Please input a number!");
             scanner.next();
         }
         input = scanner.nextInt();
         return input;
     }
 
-    //check if the input is integer and is in range
-    public static int validateInt(String prompt, int min, int max) {
-        int input;
-        do {
-            System.out.print(prompt);
-            while (!scanner.hasNextInt()) {
-                System.out.print(prompt);
-                scanner.next();
-            }
-            input = scanner.nextInt();
-        } while (!((input >= min) && (input <= max)));
-        return input;
-    }
-
-    //check if the id is existed
-    public static int validateLeadID(List<Lead> leadList) {
-        int id = validateInt("Type in the ID (number only): ");
-        while (true) {
-            for (Lead lead : leadList) {
-                if (lead.getId() == id) {
-                    return id;
-                }
-            }
-            id = validateInt("The id is not existed, type another: ");
-        }
-    }
-
     //validate name
     public static String validateName(String prompt) {
-        String result = scanner.nextLine();
+        String result = prompt;//scanner.nextLine();
         while (!result.matches("^[a-zA-Z\\s]+")) {
-            System.out.print(prompt);
+            System.out.print("Please don't use special character in name");
             result = scanner.nextLine();
+        }
+        return result;
+    }
+    //validate Student Id
+    public static String validateStudentId(String prompt) {
+        String result = scanner.nextLine();
+        while (!result.matches("^(s|S)[0-9]{7}")) {
+            System.out.print("Invalid student ID.(e.g s3836387)");
+            result = scanner.nextLine();
+        }
+        return result;
+    }
+
+    public static String validateSem() {
+        String result = scanner.nextLine();
+        boolean isValid = false;
+        while (!isValid) {
+            while (!result.matches("^[0-9]{4}(A|B|C)$")) {
+                System.out.print("Invalid semester format or semester (A,B,C). Please capitalize last letter.(e.g 2020A)");
+                result = scanner.nextLine();
+            }
+            LocalDate year = LocalDate.parse(result.substring(0,4)+"-01-01");
+            if (year.compareTo(LocalDate.of(2001,1,1))>0){
+                isValid = true;
+            }else{
+                System.out.println("Please input a valid year! (After 2001-01-01)");
+                result = scanner.nextLine();
+            }
         }
         return result;
     }
@@ -77,17 +77,17 @@ public class Console {
         // Parses the first date
         LocalDate valDate;
         // Parses the second date
-        LocalDate limitDate = LocalDate.parse("2000-01-01");
+        LocalDate limitDate = LocalDate.parse("2015-01-01");
         boolean isValid = false;
         while (!isValid) {
             try {
                 System.out.print(prompt);
                 result = scanner.next();
                 valDate = LocalDate.parse(result);
-                if (valDate.compareTo(limitDate)>=0 && valDate.compareTo(LocalDate.now())<=0){
+                if (valDate.compareTo(limitDate)<0){
                     isValid = true;
                 }else{
-                    System.out.println("Please input a valid date! (After 2000-01-01)");
+                    System.out.println("Please input a valid date! (Before 2015-01-01)");
                 }
             } catch (DateTimeParseException e) {
                 System.out.println("Wrong date format!(yyyy-mm-dd)");
@@ -96,69 +96,6 @@ public class Console {
         }
         return result;
 
-    }
-
-    public static String validateDate(String prompt) {
-        String result = null;
-        LocalDate valDate;
-        boolean isValid = false;
-        while (!isValid) {
-            try {
-                System.out.print(prompt);
-                result = scanner.next();
-                valDate = LocalDate.parse(result);
-                if (valDate.isBefore(LocalDate.now())){
-                    isValid = true;
-                }else{
-                    System.out.println("Cannot enter date after present time! Please re-enter date: ");
-                }
-            } catch (DateTimeParseException e) {
-                System.out.println("Wrong date format!(yyyy-mm-dd)");
-                isValid = false;
-            }
-        }
-        return result;
-    }
-
-    //check if input match true or false
-    public static String validateGender(String prompt) {
-        System.out.print(prompt);
-        String result = scanner.next();
-        while (!(result.equals("true") || result.equals("false"))) {
-            System.out.print(prompt);
-            result = scanner.next();
-        }
-        return result;
-    }
-
-    public static String validateEmail(String prompt) {
-        System.out.print(prompt);
-        String result = scanner.next();
-        while (!result.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
-            System.out.print("Invalid format, type again: ");
-            result = scanner.next();
-        }
-        return result;
-    }
-
-    public static String validatePhone(String prompt) {
-        System.out.print(prompt);
-        String result = scanner.next();
-        while (!result.matches("^\\d{10}$")) {
-            System.out.print("Invalid format, try again: ");
-            result = scanner.next();
-        }
-        return result;
-    }
-
-    public static String validateLead(String prompt) {
-        System.out.print(prompt);
-        String result = scanner.next();
-        while (!result.matches("^\\d{10}$")) {
-            System.out.print("Invalid format, try again: ");
-            result = scanner.next();
-        }
-        return result;
     }
 
 
